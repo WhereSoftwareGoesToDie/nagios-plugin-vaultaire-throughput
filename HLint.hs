21535e6d1a23f97ead7501eb27d8ee4a5a50d0fa
Thomas Sutton <me@thomas-sutton.id.au>
2015-03-03 11:33:42 +1100

    Add stylish and lint files

---
+++ HLint.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE PackageImports #-}
+module HLint.HLint where
+
+import "hint" HLint.Builtin.All
+import "hint" HLint.Default
+import "hint" HLint.Dollar
+import "hint" HLint.Generalise
+
+ignore "Use if"
+ignore "Use liftM"
